76b37c8b2444c59fc94e887ef3ea8965e4845990
Marrko88 <markianec@mail.ru>
2025-06-25 15:22:04 +0300

    test commit

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-https://glbs.io/api/supplies-search/?api-key=BUiYs12pra&method=search&period_start=2024-11-25&period_finish=2024-11-30&search[direction]=%D0%98%D0%9C&search[hs_code][0]=2201%2A&search[hs_code][1]=2208%2A&fields_view[0]=direction&fields_view[1]=sender_name&fields_view[2]=recipient_name&fields_view[3]=country_origin&fields_view[4]=producer
+https://glbs.io/api/supplies-search/?api-key=BUiYs12pra&method=search&period_start=2024-11-25&period_finish=2024-11-30&search[direction]=%D0%98%D0%9C&search[hs_code][0]=2201%2A&search[hs_code][1]=2208%2A&fields_view[0]=direction&fields_view[1]=sender_name&fields_view[2]=recipient_name&fields_view[3]=country_origin&fields_view[4]\=producer
+
+"new test"
